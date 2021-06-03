e5470200828b185069064798960cbfedfec5a407
chxlik <79369013+chxlik@users.noreply.github.com>
2021-06-03 16:15:24 +0300

    Add files via upload

---
+++ JS_HW1.js
@@ -0,0 +1,112 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a = 'hello'
+// console.log(a);
+// let b = 'owu';
+// alert(b)
+// let c = 'com';
+// console.log(c);
+// let d = 'ua';
+// document.write(d);
+// let num1 = 1;
+// console.log(num1);
+// let num2 = 10;
+// document.write(num2);
+// let num3 = -999;
+// alert(num3)
+// let num4 = 123;
+// console.log(num4);
+// let num5 = 3.14;
+// document.write(num5);
+// let num6 = 2.7;
+// alert(num6);
+// let num7 = 16;
+// console.log(num7);
+// let boolean1 = true;
+// alert(boolean1);
+// let boolean2 = false;
+// document.write(boolean2);
+//
+//
+// - Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// a = 'hi';
+// console.log(a);
+// b = 'hello';
+// console.log(b);
+// c = 'cucumber'
+// console.log(c)
+// d = 'job'
+// console.log(d)
+// num1 = 3
+// console.log(num1);
+// num2 = 88;
+// console.log(num2);
+// num3 = 90;
+// console.log(num3);
+// num4 = 4.66;
+// console.log(num4);
+// num5 = 8880;
+// console.log(num5);
+// num6 = 908.6;
+// console.log(num6);
+// num7 = 3.111;
+// console.log(num7);
+// boolean1 = false;
+// console.log(boolean1);
+// boolean2 = true;
+// console.log(boolean2);
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. Зконкатенувати їх в одну змінну person.
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// const a = prompt('Введите имя')
+// const b = prompt('Введите отчество')
+// const c = prompt('Введите возраст')
+// alert(`Здравствуйте ${a} ${b}, вам ${c}`)
+// console.log(a,b,c)
+
+
+
+
+
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+//
+// console.log(typeof b);
+//
+// console.log(typeof c)
+
+
+// - Поставет відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразаї не використовувати однакові оператори!!!
+//     5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+
+// 5 > 6 -> false
+// 5 >= 6 -> false
+// 5 = 6 -> false
+// 10 => 10 -> true
+// 10 = 10 -> true
+// 10 > 10 -> false
+// 10 < 10 -> false
+// 10 != 10 -> false
+// 123 > '123' -> false
+// 123 = '123' -> true
+
